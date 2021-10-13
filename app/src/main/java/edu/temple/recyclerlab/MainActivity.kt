88fae4775fbdf8433b5c1241c89c40e1fe39302b
Borat Tan <tue83853@temple.edu>
2021-10-13 14:55:37 -0400

    Lab 5!

---
+++ app/src/main/java/edu/temple/recyclerlab/MainActivity.kt
@@ -0,0 +1,22 @@
+package edu.temple.recyclerlab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val cardTitles : Array<String> = resources.getStringArray(R.array.cardTitles)
+        val cardImages: Array<String> = resources.getStringArray(R.array.cardImages)
+
+        val adapter = GridItemAdapter(cardTitles,cardImages)
+        val gridLayout = GridLayoutManager(this, 3)
+        gridItems.layoutManager = gridLayout
+        gridItems.adapter = adapter
+    }
+}
